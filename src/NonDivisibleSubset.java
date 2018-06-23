99008bfca3820e783b7cf956f8eb140d5f6b2ee3
Wenquan Zou <wenquan.zou16@imperial.ac.uk>
2018-06-23 02:09:36 +0100

    NonDivisibleSubset Solution

---
+++ src/NonDivisibleSubset.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NonDivisibleSubset {
+  static int nonDivisibleSubset(int k, int[] arr) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int k = in.nextInt();
+    int[] arr = new int[n];
+    for(int arr_i = 0; arr_i < n; arr_i++){
+      arr[arr_i] = in.nextInt();
+    }
+    int result = nonDivisibleSubset(k, arr);
+    System.out.println(result);
+    in.close();
+  }
+}
---
+++ src/SuperDigit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SuperDigit {
+
+  static int digitSum(String n, int k) {
+    // Complete this function
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String n = in.next();
+    int k = in.nextInt();
+    int result = digitSum(n, k);
+    System.out.println(result);
+    in.close();
+  }
+}
